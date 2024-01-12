4fbfe58c4e9b91748a5d40304fbbbb760c57dabe
Pudv95 <vivekupadhayay18@gmail.com>
2024-01-12 20:33:17 +0530

    remove users with deleted account

--- fetch.py
+++ fetch.py
@@ -47,27 +47,36 @@ def get_user_contest_ranking_info(username):
     response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()
-        contestList = data['data']['userContestRankingHistory']
-        count = 2
-        mostRecentContest = {'rating': 1500,'ranking': 25000}
-        prevContest = {'rating': 1500}
-        for contest in reversed(contestList):
-            if contest['attended'] == True:
-                if count == 2:
-                    mostRecentContest = contest
-                elif count == 1:
-                    prevContest = contest
-                count-=1
-                if count == 0 : 
-                    break
-        data =  {
-            'ratingChange' : mostRecentContest['rating']-prevContest['rating'],
-            'prevRank': mostRecentContest['ranking'],
-            'prevRating' : mostRecentContest['rating'],
-            'attemptedContest' : data['data']['userContestRanking']['attendedContestsCount']
-        }
-        print("done")
-        return data
+        try:
+            data = response.json()
+            contestList = data['data']['userContestRankingHistory']
+            count = 2
+            mostRecentContest = {'rating': 1500,'ranking': 25000}
+            prevContest = {'rating': 1500}
+            for contest in reversed(contestList):
+                if contest['attended'] == True:
+                    if count == 2:
+                        mostRecentContest = contest
+                    elif count == 1:
+                        prevContest = contest
+                    count-=1
+                    if count == 0 : 
+                        break
+            data =  {
+                'ratingChange' : mostRecentContest['rating']-prevContest['rating'],
+                'prevRank': mostRecentContest['ranking'],
+                'prevRating' : mostRecentContest['rating'],
+                'attemptedContest' : data['data']['userContestRanking']['attendedContestsCount']
+            }
+            print('Added Succesfully')
+            return data
+        except:
+            print('Got some error')
+            return {
+                'ratingChange' : '-1',
+                'prevRank': '-1',
+                'prevRating' : '-1',
+                'attemptedContest' : '-1'
+            }
     else:
         print("Error:", response.status_code, response.text)
--- main.py
+++ main.py
@@ -11,40 +11,45 @@ chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options)
 
 try:
+    for contest in range(379,300,-1):
+        for index in range(100):
+            url = 'https://leetcode.com/contest/weekly-contest-'+str(contest)+'/ranking/' + str(index+1) + '/'
+            driver.get(url)
+            print(url)
 
-    for index in range(1):
-        url = 'https://leetcode.com/contest/weekly-contest-379/ranking/' + str(index+1) + '/'
-        driver.get(url)
-        print(url)
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '.table-responsive'))
+            )
 
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.table-responsive'))
-        )
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '.table-responsive tbody tr'))
+            )
 
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.table-responsive tbody tr'))
-        )
+            table = driver.find_element(By.CSS_SELECTOR, '.table-responsive')
 
-        table = driver.find_element(By.CSS_SELECTOR, '.table-responsive')
+            table_text = table.text
 
-        table_text = table.text
-
-        rows = [row.split()[:3] for row in table_text.split('\n')]
-        rows[0].append('ratingChange')
-        rows[0].append('prevRank')
-        rows[0].append('prevRating')
-        rows[0].append('attemptedContest')
-        for user in rows[1:]:
-            newdata = get_user_contest_ranking_info(user[1])
-            user.append(newdata['ratingChange'])
-            user.append(newdata['prevRank'])
-            user.append(newdata['prevRating'])
-            user.append(newdata['attemptedContest'])
-        if index > 0:
-            rows.pop(0)
-        with open('table_data.csv', 'a', newline='', encoding='utf-8') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_writer.writerows(rows)
+            rows = [row.split()[:3] for row in table_text.split('\n')]
+            rows[0].append('ratingChange')
+            rows[0].append('prevRank')
+            rows[0].append('prevRating')
+            rows[0].append('attemptedContest')
+            finalData = []
+            for user in rows[1:]:
+                newdata = get_user_contest_ranking_info(user[1])
+                user.append(newdata['ratingChange'])
+                user.append(newdata['prevRank'])
+                user.append(newdata['prevRating'])
+                user.append(newdata['attemptedContest'])
+                if user[4] == '-1':
+                    continue
+                else:
+                    finalData.append(user)
+            if index == 0:
+                finalData.insert(0,rows[0])
+            with open('table_data.csv', 'a', newline='', encoding='utf-8') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerows(finalData)
 
 finally:
     driver.quit()
